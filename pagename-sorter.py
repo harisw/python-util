fd6c912f796254825616d9734f7288cfb691e066
DBLab <DBLab@DESKTOP-IQGPJN3>
2022-02-10 13:11:52 +0900

    Added argument for script

--- pagename-sorter.py
+++ pagename-sorter.py
@@ -1,7 +1,6 @@
 import os
 import glob
 import sys
-#target_folder = 'C:/Users/DBLab/Downloads/deathinthefamily'
 target_folder = sys.argv[1]
 fileList = glob.glob(target_folder+'/*.jpg')
 formatTarget = '0'+str(len(str(bin(int(len(fileList)))))) +'b'
