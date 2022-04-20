58870aacbd2e8e5b3c05e938098eb8cce9ab0fac
harisw <hari14@mhs.if.its.ac.id>
2022-04-21 02:20:41 +0900

    use css selector for img scraper

--- recipe-img-scraper.py
+++ recipe-img-scraper.py
@@ -1,17 +1,4 @@
 from selenium import webdriver
-url = 'https://www.google.com/search?tbm=isch&q='
-DRIVER_PATH = './chromedriver'
-# driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-# browser = driver.get(url+'Campfire Orange Cake')
-# delay = 3 # seconds
-# try:
-#     myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="islrg"]/div[1]/div[1]')))
-#     print("Page is ready!")
-# except TimeoutException:
-#     print("Not found")
-# finally:
-#     driver.quit() 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,37 +6,58 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.options import Options
 import urllib
 import time
-start = time.time()
+
+url = 'https://www.google.com/search?tbm=isch&q='
+DRIVER_PATH = './chromedriver'
 opt = Options()
 opt.headless=True
-driver = webdriver.Firefox(options=opt)
-driver.get(url+'Parfatti pizza')
-xpathQuery = '//*[@id="islrg"]/div[1]/div[1]/a[1]'
-secQuery = '//*[@id="Sva75c"]/div'
-#CLick element first
-try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, xpathQuery))
-    )
-    element.click()
-    checkEl = WebDriverWait(driver, 2).until(
-        EC.presence_of_element_located((By.XPATH, secQuery))
-    )
-    element = WebDriverWait(driver, 2).until(
-        EC.presence_of_element_located((By.XPATH, xpathQuery))
-    )
 
-    hrefResult = element.get_attribute('href').replace('/imgres?imgurl=', '')
-    unquoted = urllib.unquote(hrefResult).decode('utf8').replace('https://www.google.com', '')
-    filtered = unquoted.split('&imgrefurl')
-    print("filtered "+filtered[0])
-    end = time.time()
-    dur = end - start
-    print("Elapsed: ", dur)
-except TimeoutException:
-    print("timeout")
-except Exception as e:
-    print(e)
-finally:
-    driver.quit()
-    exit()
+def scrape_img(keyword):
+    driver = webdriver.Firefox(options=opt)
+    driver.get(url+keyword)
+    xpathQuery = '//*[@id="islrg"]/div[1]/div[1]/a[1]'
+    cssQuery = '#islrg > div.islrc > div:nth-child(2) > a.wXeWr.islib.nfEiy'
+    secQuery = '//*[@id="Sva75c"]/div'
+    cssSecQuery = '#Sva75c > div'
+                
+    #CLick element first
+    filtered = ''
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, cssQuery))
+        )
+        element.click()
+
+        checkEl = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, cssSecQuery))
+        )
+        element = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, cssQuery))
+        )
+
+        hrefResult = element.get_attribute('href').replace('/imgres?imgurl=', '')
+        unquoted = urllib.unquote(hrefResult).decode('utf8').replace('https://www.google.com', '')
+        filtered = unquoted.split('&imgrefurl')[0]
+        
+    except TimeoutException:
+        print("timeout")
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+        return filtered
+
+def scrape_func(row):
+    row['image'] = scrape_img(row['name'])
+    return row
+
+
+import dask.dataframe as dd
+from dask.diagnostics import ProgressBar
+import pandas as pd
+
+ProgressBar().register()
+df = pd.read_csv('./100k_preprocessed_recipes.csv')[:10000]
+ddf = dd.from_pandas(df, npartitions=10) # find your own number of partitions
+ddf_update = ddf.apply(scrape_func, axis=1).compute()
+ddf_update.to_csv('../datasets/recipes_with_img', single_file=True)
